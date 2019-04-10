bad2880448abcd4d58c1e19468f0a854612b2cf8
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-10 18:46:04 -0300

    feat: imported api and added handleSubmit method

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
 export default class Main extends Component {
+  /**
+   * Contains all data that
+   * is handled by this component.
+   *
+   * Everytime data is changed in state
+   * component's render method is called.
+   */
+  state = {
+
+  };
+
+  /**
+   * Method for handling data
+   * submission from form.
+   */
+  handleSubmit = () => {
+
+  }
+
   render() {
       return(
         <div id="main-container">
-          <form action="">
+          <form onSubmit={this.handleSubmit}>
             <img src={logo} alt=""/>
             <input placeholder="Criar um box" />
             <button type="submit">Criar</button>
