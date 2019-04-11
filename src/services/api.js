437b595a486a323b03d56f567685a41f2cb0892d
Matheus Crivellari <matheus.acrivellari@gmail.com>
2019-04-11 01:50:44 -0300

    fix: baseUrl scure protocol

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL : 'http://omnistack-teco.herokuapp.com',
+    baseURL : 'https://omnistack-teco.herokuapp.com',
 });
 
 export default api;
